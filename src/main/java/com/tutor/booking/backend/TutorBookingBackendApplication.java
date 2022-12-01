17a6377d7b3792d7a62ccd25ddfd3318c2601b99
naskavinda <naskavinda@gmail.com>
2022-12-01 09:53:53 +0530

    initial commit

---
+++ src/main/java/com/tutor/booking/backend/TutorBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tutor.booking.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutor/booking/backend/TutorBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutor.booking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
